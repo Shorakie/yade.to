362eb9b2e42ef16809e16a5c1378764006236c9c
Shorakie <mhmdamin.jafari@gmail.com>
2024-06-15 07:59:03 +0330

    feat: nextjs

---
+++ frontend/yadeto/postcss.config.js
@@ -0,0 +1,11 @@
+const { join } = require('path');
+
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.ts')
+    },
+    autoprefixer: {},
+  },
+};
