f076b4edc2d8242ebc457b941406c9cf5bf371c9
Shorakie <mhmdamin.jafari@gmail.com>
2024-06-15 07:59:03 +0330

    feat: nextjs

---
+++ frontend/yadeto/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+import { join } from "path";
+
+const config: Config = {
+  content: [
+    join(__dirname, '{pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,mdx}'),
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
