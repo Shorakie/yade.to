e4f3bcc36b62407e0dc2f293486cfa90e22eb7b4
Shorakie <mhmdamin.jafari@gmail.com>
2024-06-15 08:40:16 +0330

    feat: add jest

---
+++ frontend/yadeto/jest.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default {
+  displayName: "yadeto",
+  preset: "../../jest.config.ts",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/frontend",
+};
